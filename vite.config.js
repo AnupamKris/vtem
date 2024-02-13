c4c551b09abcf70ec93c61ea3fc9dc62e87a3a64
AnupamKris <anupamkris13262@gmail.com>
2024-02-13 15:47:23 +0530

    bugs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components";
-import AutoImport from "unplugin-auto-import";
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
